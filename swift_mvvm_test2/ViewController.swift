98e0842a115b62efa419984558fafd00bb24da53
Poonsak Aphidetmongkhon <poonsak_lds@hotmail.com>
2021-04-21 23:57:37 +0700

    Initial Commit

---
+++ swift_mvvm_test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_mvvm_test2
+//
+//  Created by Poonsak Aphidetmongkhon on 21/4/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
